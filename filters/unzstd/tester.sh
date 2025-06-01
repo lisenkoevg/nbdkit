9ad70e32b676cbccbe570191c272b1cf11626e3b
Лысенко Евгений <evgeen@yandex.ru>
2025-06-01 13:56:58 +0300

    v1

--- filters/unzstd/tester.sh
+++ filters/unzstd/tester.sh
@@ -4,38 +4,39 @@
 # root_dir/
 #   nbdkit/ - nbdkit src dir
 #   libnbd/ - libnbd src dir
-#   nbdkit_build/ - dir where configure was made
+#   nbdkit_build/ - dir where nbdkit's `./configure` was made
 
 root_dir=$HOME/work2
 nbdkit_build_dir=$root_dir/nbdkit_build
 # libnbd failed to `make` if `./congigure`-d in another dir
 libnbd_build_dir=$root_dir/libnbd
+ZSTD=$root_dir/nbd/experiments/myzstd
 
 cd $libnbd_build_dir/copy
 make -q || { make && sudo make install && printf "\n"; }
 [ $? != 0 ] && exit 1
 
 cd $nbdkit_build_dir/filters/unzstd
-make -q || { CFLAGS="-Werror -Wfatal-errors" make -e && sudo make install && printf "\n"; }
+make -q || { CFLAGS="-Wno-unused-variable -Wno-unused-function -Wall -Werror -Wfatal-errors" make -e && sudo make install && printf "\n"; }
 [ $? != 0 ] && exit 1
 
 cd $root_dir
 
 sample_size=${1:-1}
 koef=${2:-14}
-# sample_data=$(echo -n $(yes '\x1' | head -n $sample_size) | sed -E 's/\s+//g')
-dd if=/dev/random of=sample bs=1 count=$sample_size status=none
-file_size=$((sample_size * koef))
+echo -en $(yes '\x1' | head -n $sample_size) | sed -E 's/\s+//g' > sample
+# dd if=/dev/random of=sample bs=1 count=$sample_size status=none
+file_size=$((sample_size * koef + 2 + 16))
+# file_size=$((sample_size))
 
 t1=$(date +%s%3N)
 
-zstd < sample > sample.zt \
+$ZSTD < sample > sample.zt \
   && dd if=/dev/zero of=file.img bs=1 count=$file_size status=none \
-  && nbdkit $V1 -P nbdkit.pid --filter=log -D unzstd.flag=1 --filter=unzstd file file.img logfile=nbdkit.log \
-  && cat sample.zt | nbdcopy - nbd://localhost \
+  && nbdkit $V2 -P nbdkit.pid --filter=log -D unzstd.flag=1 --filter=unzstd file file.img logfile=nbdkit.log \
+  && cat sample | nbdcopy $V1 - nbd://localhost \
   && { diff -qs <(head -c $sample_size file.img) sample > /dev/null && echo Test passed || echo Test FAILED; } \
-  ; kill $(cat nbdkit.pid) > /dev/null 2>&1 \
-  ; rm -rf nbdkit.pid
+  ; kill $(cat nbdkit.pid) > /dev/null 2>&1 ; rm -rf nbdkit.pid
 
 t2=$(date +%s%3N)
 echo "elapsed $((t2 - t1))ms"
--- filters/unzstd/unzstd.c
+++ filters/unzstd/unzstd.c
@@ -21,101 +21,29 @@
 #include "minmax.h"
 #include "utils.h"
 
+#include "../../../nbd/experiments/dump_buffer.c"
+
 int unzstd_debug_flag;
-size_t bufOutSize;
-void *bufOut;
-ZSTD_DCtx *dctx;
-FILE *fdebug;
 
-static const char *entry; /* File within tar (tar-entry=...) */
-static int64_t tar_limit = 0;
-static const char *tar_program = "tar";
-
-/* Offset and size within tarball.
- *
- * These are calculated once in the first connection that calls
- * tar_prepare.  They are protected by the lock.
- */
-static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-static bool initialized = false;
-static uint64_t tar_offset, tar_size;
-
-int zstd_init() {
-  bufOutSize = ZSTD_DStreamOutSize();
-  nbdkit_debug("bufOutSize: %lu", bufOutSize);
-  dctx = ZSTD_createDCtx();
-  if (dctx == NULL)
-    return 1;
-  bufOut = malloc(bufOutSize);
-  if (bufOut == NULL)
-    return 1;
-  fdebug =
-      fopen("/home/evgen/work/nbdkit/build/filters/unzstd/unzstd.out", "w");
-  if (!fdebug)
-    return 1;
-  return 0;
-}
-
-int zstd_finalize() {
-  ZSTD_freeDCtx(dctx);
-  free(bufOut);
-  fclose(fdebug);
-  return 0;
-}
-
-void zstd_decompress() {}
-
-static int tar_config(nbdkit_next_config *next, nbdkit_backend *nxdata,
+static int unzstd_config(nbdkit_next_config *next, nbdkit_backend *nxdata,
                       const char *key, const char *value) {
-  if (strcmp(key, "tar-entry") == 0) {
-    if (entry) {
-      nbdkit_error("only one tar-entry parameter can be given");
-      return -1;
-    }
-    entry = value;
-    return 0;
-  } else if (strcmp(key, "tar-limit") == 0) {
-    tar_limit = nbdkit_parse_size(value);
-    if (tar_limit == -1)
-      return -1;
-    return 0;
-  } else if (strcmp(key, "tar") == 0) {
-    tar_program = value;
-    return 0;
-  }
-
   return next(nxdata, key, value);
 }
 
-static int tar_config_complete(nbdkit_next_config_complete *next,
+static int unzstd_config_complete(nbdkit_next_config_complete *next,
                                nbdkit_backend *nxdata) {
-  if (entry == NULL) {
-    nbdkit_error("you must supply the tar-entry=<FILENAME> parameter");
-    return -1;
-  }
-
   return next(nxdata);
 }
 
-#define tar_config_help                                                        \
-  "tar-entry=<FILENAME> (required) The path inside the tar file to serve.\n"   \
-  "tar-limit=SIZE                  Limit on reading to find entry.\n"          \
-  "tar=<PATH>                      Path of the tar binary."
-
 struct handle {
-  /* These are copied from the globals during tar_prepare, so that we
+  /* These are copied from the globals during unzstd_prepare, so that we
    * don't have to keep grabbing the lock on each request.
    */
   uint64_t offset, size;
 };
 
-static void *tar_open(nbdkit_next_open *next, nbdkit_context *nxdata,
+static void *unzstd_open(nbdkit_next_open *next, nbdkit_context *nxdata,
                       int readonly, const char *exportname, int is_tls) {
-  if (zstd_init() != 0) {
-    nbdkit_error("zstd_init");
-    return NULL;
-  }
-
   struct handle *h;
   if (next(nxdata, readonly, exportname) == -1)
     return NULL;
@@ -125,36 +53,22 @@ static void *tar_open(nbdkit_next_open *next, nbdkit_context *nxdata,
     nbdkit_error("calloc: %m");
     return NULL;
   }
-  if (unzstd_debug_flag)
-    nbdkit_debug("open");
   return h;
 }
 
-static void tar_close(void *handle) {
-  zstd_finalize();
-  if (unzstd_debug_flag)
-    nbdkit_debug("close");
+static void unzstd_close(void *handle) {
   free(handle);
 }
 
-static int tar_prepare(nbdkit_next *next, void *handle, int readonly) {
+static int unzstd_prepare(nbdkit_next *next, void *handle, int readonly) {
   struct handle *h = handle;
   h->offset = 0;
   h->size = 0;
-}
-
-/* Description. */
-static const char *tar_export_description(nbdkit_next *next, void *handle) {
-  const char *base = next->export_description(next);
-
-  if (!base)
-    return NULL;
-  return nbdkit_printf_intern("embedded %s from within tar file: %s", entry,
-                              base);
+  return 0;
 }
 
 /* Get the file size. */
-static int64_t tar_get_size(nbdkit_next *next, void *handle) {
+static int64_t unzstd_get_size(nbdkit_next *next, void *handle) {
   struct handle *h = handle;
   int64_t size;
 
@@ -162,8 +76,8 @@ static int64_t tar_get_size(nbdkit_next *next, void *handle) {
    * result, because it caches the plugin size in server/backend.c.
    */
   size = next->get_size(next);
-  if (unzstd_debug_flag)
-    nbdkit_debug("get_size size=%lu h->size=%lu", size, h->size);
+//   if (unzstd_debug_flag)
+//     nbdkit_debug("get_size size=%lu h->size=%lu", size, h->size);
 
   if (size == -1)
     return -1;
@@ -172,48 +86,47 @@ static int64_t tar_get_size(nbdkit_next *next, void *handle) {
   return size;
 }
 
-/* Read data from the file. */
-static int tar_pread(nbdkit_next *next, void *handle, void *buf, uint32_t count,
-                     uint64_t offs, uint32_t flags, int *err) {
-  struct handle *h = handle;
-  return next->pread(next, buf, count, offs + h->offset, flags, err);
-}
-
 /* Write data to the file. */
-static int tar_pwrite(nbdkit_next *next, void *handle, const void *buf,
+static int unzstd_pwrite(nbdkit_next *next, void *handle, const void *buf,
                       uint32_t count, uint64_t offs, uint32_t flags, int *err) {
-  struct handle *h = handle;
+  struct {
+    size_t size;
+    uint64_t offset;
+  } orig;
 
-  size_t const ret = ZSTD_decompress(bufOut, bufOutSize, buf, count);
-  if (ZSTD_isError(ret)) {
-    // TODO handle error
-    if (unzstd_debug_flag)
-      nbdkit_debug("ZSTD_isError: %s", ZSTD_getErrorName(ret));
+  memcpy(&orig, buf, sizeof orig);
+
+  void *bufOut = malloc(orig.size);
+  if (bufOut == NULL) {
+    nbdkit_error("unzstd malloc");
+    return -1;
   }
-  size_t unzstd_offs = h->offset;
-  uint32_t unzstd_count = ret;
-  h->offset += ret;
+
+  size_t const ret = ZSTD_decompress(bufOut, orig.size, buf + sizeof orig, count - sizeof orig);
+  if (ZSTD_isError(ret)) {
+    fprintf(stderr, "ZSTD_decompress() error: %s\n, count=%u\n", ZSTD_getErrorName(ret), count);
+    nbdkit_error("%s", ZSTD_getErrorName(ret));
+    return -1;
+  }
+#if 0
+  char *s = buffer_to_str_wrap(buf, count, 0);
+  nbdkit_debug("%s", s);
+#endif
+  free((void *)buf);
 
   if (unzstd_debug_flag)
-    nbdkit_debug("decompress, ret=%lu, input offs/count:%lu/%u unzstd offs/count:%lu/%u, bufOutSize %lu",
-        ret, offs, count, unzstd_offs, unzstd_count, bufOutSize);
-  return next->pwrite(next, bufOut, unzstd_count, unzstd_offs, flags, err);
+    nbdkit_debug("decompress, ret=%lu, compressed size: %u, orig size/offset:%lu/%lu",
+        ret, count, orig.size, orig.offset);
+  return next->pwrite(next, bufOut, orig.size, orig.offset, flags, err);
 //   return next->pwrite(next, buf, count, offs, flags, err);
 }
 
 static struct nbdkit_filter filter = {
     .name = "unzstd",
-    //   .longname           = "nbdkit unzstd filter",
-    //   .config             = tar_config,
-    //   .config_complete    = tar_config_complete,
-    //   .config_help        = tar_config_help,
-    .open = tar_open,
-    .close = tar_close,
-    //   .prepare            = tar_prepare,
-    //   .export_description = tar_export_description,
-    .get_size = tar_get_size,
-    //   .pread = tar_pread,
-    .pwrite = tar_pwrite,
+    .open = unzstd_open,
+    .close = unzstd_close,
+    .get_size = unzstd_get_size,
+    .pwrite = unzstd_pwrite,
 };
 
 NBDKIT_REGISTER_FILTER(filter)
